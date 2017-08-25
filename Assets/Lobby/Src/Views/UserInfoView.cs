9ec1c800414514990105f4b09d351131c3521f0f
suxingdong <sxd616616@163.com>
2017-08-25 14:53:25 +0800

    用户信息

---
+++ Assets/Lobby/Src/Views/UserInfoView.cs
@@ -0,0 +1,22 @@
+/***********************************************
+	FileName: UserInfoView.cs	    
+	Creation: 2017-08-25
+	Author：East.Su
+	Version：V1.0.0
+	Desc: 
+**********************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInfoView : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
